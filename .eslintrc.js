78234753426b2b5cd318ec1e993ce741f1369ef0
ks2400 <kscobblee2400@naver.com>
2026-03-25 01:07:30 +0900

    test
    기본적인 파일 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
     "rules" : {
         "semi" : ["error", "always"],
         "quotes" : ["error", "double"],
+        "no-unused-vars": "warn",
     },
     "env" : {
         "es6" : true,
+        "node" : true,
     },
 };
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 26-1_Capstone_Design
+## 졸업작품으로 만들어지는 프로젝트입니다.
+
+### 사용 스택
+1. JavaScript
+2. Node.js
+3. Discord.js
+4. ESLint
